d1796b07f6cbc0d3ed50b6042735c24131213844
mvsaturno <mv_saturno@yahoo.com.br>
2014-10-14 00:15:02 -0300

    Cotação2

--- cotacoes.php
+++ cotacoes.php
@@ -29,100 +29,83 @@
                         
                         <div class="div_cot_form_left grid_5">
                         
-                            <table style="width: 100%;">
+                            <div>
+                            <label for="nome">Nome/Empresa:</label>
+                            <input type="text" name="nome" id="nome" class="alpha grid_5" />
+                            </div>
 
-                                <tr>
-                                    <td><label for="nome">Nome/Empresa:</label></td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2"><input type="text" name="nome" id="nome" /></td>
-                                </tr>
+                            <div>
+                            <label for="email">Email:</label>
+                            <input type="text" name="email" id="email" class="alpha grid_3" />
+                            
+                            <label for="tel">Telefone:</label>
+                            <input type="text" name="tel" id="tel" class="alpha grid_3" />
+                            </div>
 
-                                <tr>
-                                    <td>
-                                        <label for="email">Email:</label>
-                                    </td>
-                                    <td>
-                                        <label for="tel">Telefone:</label>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <input type="text" name="email" id="email" />
-                                    </td>
-                                    <td>
-                                        <input type="text" name="tel" id="tel"/>
-                                    </td>
-                                </tr>
-                            </table>
-                            
-                            
-                            
-
-                            
+                            <div>
                             <label for="origem">Origem:</label>
                             <input type="text" name="origem" id="origem" class="alpha grid_5" />
-                            
+                            </div>
 
-                            
+                            <div>
                             <label for="destino">Destino:</label>
                             <input type="text" name="destino" id="destino" class="alpha grid_5" />
-                            
+                            </div>
 
-                            
+                            <div>
                             <label for="destinatario">Destinatário:</label>
                             <input type="text" name="destinatario" id="destinatario" class="alpha grid_5" />
-                            
+                            </div>
 
-                            
+                            <div>
                             <label for="descricao">Descricao:</label>
                             <textarea name="descricao" id="descricao" class="alpha grid_5"></textarea>
-                            
+                            </div>
 
                         </div>
 
                         <div class="div_cor_form_right grid_6">
                         
-                        
+                        <div>
                         <label for="valor">Valor Declarado:</label>
                         <input type="text" name="valor" id="valor" class="alpha grid_4" />
-                        
+                        </div>
 
-                        
+                        <div>
                         <label for="seguro">Seguro Prório?</label>
                         <input type="text" name="seguro" id="seguro" class="alpha grid_2" />
-                        
+                        </div>
 
-                        
+                        <div>
                         <label for="peso_bruto">Peso Bruto(kg):</label>
                         <input type="text" name="peso_bruto" id="peso_bruto" class="alpha grid_3" />
-                        
+                        </div>
 
-                        
+                        <div>
                         <label for="qtde_volumes">Quantidade de Volumes:</label>
                         <input type="text" name="qtde_volumes" id="qtde_volumes" class="alpha grid_3" />                        
-                        
+                        </div>
 
-                        
+                        <div>
                         <label for="largura">Largura:</label>
                         <input type="text" name="largura" id="largura" class="alpha grid_2" />
-                        
+                        </div>
 
-                        
+                        <div>
                         <label for="altura">Altura:</label>
                         <input type="text" name="altura" id="altura" class="alpha grid_2" />
-                        
+                        </div>
 
-                        
+                        <div>
                         <label for="comprimento">Comprimento:</label>
                         <input type="text" name="comprimento" id="comprimento" class="alpha grid_2" />
-                        
+                        </div>
 
-                        
+                        <div>
                         <label for="peso_cubado">Peso Cubado:</label>
                         <input type="text" name="peso_cubado" id="peso_cubado" class="alpha grid_3" />
                         <img src="img/calc.png" />
-                        
+                        </div>
 
                         <div class="form_buttons grid_4">
                             <button class="alpha grid_2">Cancelar</button>
